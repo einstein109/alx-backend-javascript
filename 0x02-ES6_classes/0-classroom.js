7f98d9112ed784529aa51bb70c7cf1cd978fc020
einstein109 <cheruiyoteinstein@gmail.com>
2023-09-28 11:52:41 +0000

    hard things

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a class room.
+ */
+export default class classroom {
+	/**
+	 * creates a new @see {@link Classroom}.
+	 * @param {Number} maxStudentSize - The maximum number of
+	 * students in the class
+	 */
+	constructor(maxStudentsSize) {
+		this._maxStudentSize = maxStudentsSize:
+	      }
+}
